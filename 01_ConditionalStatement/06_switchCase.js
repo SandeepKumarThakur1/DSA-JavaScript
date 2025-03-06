eaebeedcd847bc13aaeeab92662646113ca7f341
sandeep Kumar <sandeepkumarthakur9888@gmail.com>
2025-03-06 21:46:29 +0530

    add switch case file

--- 01_ConditionalStatement/06_switchCase.js
+++ 01_ConditionalStatement/06_switchCase.js
@@ -9,20 +9,55 @@
 
 // Example:
 
-let day = 3;
+let day = 1;
 
 switch (day) {
-    case 1:
-        console.log("Sunday"); // down fall through
-        
-    case 2:
-        console.log("Monday");
-        break;
-        
-    case 3:
-        console.log("Tuesday");
-        break;
+  case 1:
+    console.log("Sunday"); // fall through condition
+  case 2:
+    console.log("Monday");
+    break;
+  case 3:
+    console.log("Tuesday");
+    break;
 
-    default:
-        console.log("Invalid Day");
+  default:
+    console.log("Invalid Day");
+}
+
+let month = 3;
+switch (month) {
+  case 1:
+  case 2:
+  case 3:
+    console.log("Jan");
+    break;
+
+  default:
+    console.log("Wrong Input");
+}
+
+switch (true) {
+  case 19 < 6:
+    console.log("Hello");
+    break;
+  case 19 > 8:
+    console.log("hey");
+    break;
+
+  default:
+    console.log("Invalid");
+}
+
+let num = 0.2 + 0.8;
+switch (num) {
+  case 0.4:
+    console.log("hello");
+    break;
+  case 0.5:
+    console.log("hey");
+    break;
+
+  default:
+    console.log("namaste");
 }
